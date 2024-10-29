b39eebacd05927e5bb0eb05f20f108a2b468377d
RC_Student_lab <RC_Student_lab@10.0.1.3>
2024-10-29 20:01:54 +0000

    Add unit test for returnTotalHours method with expected result of 55 hours

--- src/test/java/com/mycompany/accountcreator/TaskTest.java
+++ src/test/java/com/mycompany/accountcreator/TaskTest.java
@@ -201,8 +201,13 @@ public class TaskTest {
     @Test
     public void testReturnTotalHours_3() {
         System.out.println("returnTotalHours");
+     // Define the expected total hours (55 hours) for this test case
         float expResult = 55F;
+        
+     // Call the returnTotalHours method to get the actual total hours
         float result = Task.returnTotalHours();
+        
+    // Assert that the actual total hours match the expected total hours   
         assertEquals(expResult, result, 0);
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
